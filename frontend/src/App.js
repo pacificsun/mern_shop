1b25c08c9fd28fb602c4eec0626e20191ce597b3
Suraj Thapa <stsurajst@gmail.com>
2020-11-14 22:47:46 +0545

    pre set for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div>Welcome</div>;
+  return (
+    <>
+      <Header />
+      <h1>Pro shop</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
